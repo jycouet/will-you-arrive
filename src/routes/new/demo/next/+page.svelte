8891191dee580bb044d0e95d1d040b637ebd4463
jycouet <jycouet@gmail.com>
2024-01-22 13:52:11 +0100

    okay

--- src/routes/new/demo/next/+page.svelte
+++ src/routes/new/demo/next/+page.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+  // import { route } from "$lib/ROUTES";
+
   let nb = 100;
 </script>
 
@@ -9,12 +11,14 @@
         /demo
           /next
             +page@new.svelte   // we are here
+      /HowManyWinners
+        +page.svelte
 	</pre>
 
 <input type="number" bind:value={nb} />
 <pre> &lt;a href="/new/howManyWinners?{nb}"&gt; Will I arrive ? &lt;/a&gt;</pre>
 <a href="/new/howManyWinners?nb={nb}">Will I arrive ?</a>
-
+<!-- <a href={route("/new/howManyWinners", { nb })}>Yes of course for {nb}</a> -->
 <ul>
   <li>YES: Hands up</li>
   <li>NO: Hands down</li>
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,48 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { sveltekit } from "@sveltejs/kit/vite";
+import { defineConfig } from "vite";
 
 export default defineConfig({
-	plugins: [sveltekit()]
+  plugins: [
+    sveltekit(),
+    // TODO HERE
+  ],
 });
+
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+
+//
+//
+//
+//
+// import type { KIT_ROUTES } from "$lib/ROUTES";
+//
+// import { kitRoutes } from "vite-plugin-kit-routes";
+//
+// kitRoutes<KIT_ROUTES>({
+//   PAGES: {
+//     "/new/howManyWinners": {
+//       explicit_search_params: {
+//         nb: { required: true },
+//       },
+//     },
+//   },
+// }),
